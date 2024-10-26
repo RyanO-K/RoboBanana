49b7d6a243d8d1a435f6b713463a6b39aad2b0ad
Ryan O <rokeefe135@gmail.com>
2024-10-26 16:44:55 -0400

    Update reaction_commands.py

--- commands/reaction_commands.py
+++ commands/reaction_commands.py
@@ -12,8 +12,8 @@ import os
 LOG = logging.getLogger(__name__)
 
 MOD_ROLE = os.getenv("ROLES_MOD")
-CROWD_MUTE_DURATION = os.getenv("CROWDMUTE_DURATION")
-CROWD_MUTE_THRESHOLD = os.getenv("CROWDMUTE_THRESHOLD")
+CROWD_MUTE_DURATION = int(os.getenv("CROWDMUTE_DURATION"))
+CROWD_MUTE_THRESHOLD = int(os.getenv("CROWDMUTE_THRESHOLD"))
 # these are hardcoded until raze to radiant is over, or config file changes are allowed
 # for testing on own setup, these need to be changed to your appropriate IDs
 # HIDDEN_MOD_ROLE should be 1040337265790042172 when committing and refers to the Mod (Role Hidden)
