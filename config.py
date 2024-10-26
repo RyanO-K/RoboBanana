0f9225b900116576a6f3477564d1f2cd8f0a7002
Ryan O <rokeefe135@gmail.com>
2024-10-26 15:29:10 -0400

    check env

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config:
 
 class YAMLConfig:
     CONFIG = dict()
+    LOG.ERROR(os.getenv(ROLES_MOD))
     # try:
     #     with open("/config.yaml") as config_file:
     #         CONFIG = yaml.safe_load(config_file)
