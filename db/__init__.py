17619159c30e05ff86830e5f34d1ecdeec50d12d
RyanO-K <RyanO-K@users.noreply.github.com>
2024-10-27 03:11:06 +0000

    Apply Black formatting changes

--- db/__init__.py
+++ db/__init__.py
@@ -116,7 +116,7 @@ class DB:
         password = os.getenv("DATABASE_PASSWORD")
         LOG = logging.getLogger(__name__)
 
-        if password == None or password == '':
+        if password == None or password == "":
             LOG.error("Invalid Database Password")
             LOG.error(password)
         db_host = Config.CONFIG["Database"]["Host"]
